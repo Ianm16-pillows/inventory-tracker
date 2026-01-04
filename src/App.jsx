e8ae008f9326412dfe4a3a8fc340b90a2407ac8f
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 19:28:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import Navbar from "./components/Navbar";
-import Landing from "./pages/Landing";
-import Dashboard from "./pages/Dashboard";
-import AddEditItem from "./pages/AddEditItem";
-import mockData from "./data/mockData";
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Dashboard from "./pages/Dashboard"
+import Inventory from "./pages/Inventory"
+import AddItem from "./pages/AddItem"
+import EditItem from "./pages/EditItem"
+import Alerts from "./pages/Alerts"
 
 function App() {
-  const [items, setItems] = useState(mockData);
-
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/dashboard"
-          element={<Dashboard items={items} setItems={setItems} />}
-        />
-        <Route
-          path="/add"
-          element={<AddEditItem items={items} setItems={setItems} />}
-        />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/add" element={<AddItem />} />
+        <Route path="/edit/:id" element={<EditItem />} />
+        <Route path="/alerts" element={<Alerts />} />
       </Routes>
-    </BrowserRouter>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App

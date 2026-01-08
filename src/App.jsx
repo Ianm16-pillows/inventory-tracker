07594aae86f92cba9124857c0a0cb8d44aa2eec5
Ian Mwambuki <merlnadika@gmail.com>
2026-01-08 21:07:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Inventory from "./pages/Inventory";
 import AddItem from "./pages/AddItem";
 import EditItem from "./pages/EditItem";
 import Alerts from "./pages/Alerts";
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <nav className="bg-primary text-white p-4 flex gap-6">
+        <NavLink to="/">Inventory</NavLink>
+        <NavLink to="/add">Add Item</NavLink>
+        <NavLink to="/alerts">Alerts</NavLink>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Inventory />} />
         <Route path="/add" element={<AddItem />} />
         <Route path="/edit/:id" element={<EditItem />} />
         <Route path="/alerts" element={<Alerts />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-export default App;

1c69a073e7ebbace6780f77ac644315bc2d37f73
Ian Mwambuki <merlnadika@gmail.com>
2026-01-10 15:28:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Inventory from "./pages/Inventory";
-import AddItem from "./pages/AddItem";
-import EditItem from "./pages/EditItem";
-import Alerts from "./pages/Alerts";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Inventory from './pages/Inventory.jsx';
+import AddItem from './pages/AddItem.jsx';
+import EditItem from './pages/EditItem.jsx';
+import Alerts from './pages/Alerts.jsx';
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Inventory />} />
-        <Route path="/add" element={<AddItem />} />
-        <Route path="/edit/:id" element={<EditItem />} />
-        <Route path="/alerts" element={<Alerts />} />
-      </Routes>
-    </>
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <nav className="bg-primary text-white p-4 flex gap-4">
+          <Link to="/">Inventory</Link>
+          <Link to="/add">Add Item</Link>
+          <Link to="/alerts">Alerts</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Inventory />} />
+          <Route path="/add" element={<AddItem />} />
+          <Route path="/edit" element={<EditItem />} />
+          <Route path="/alerts" element={<Alerts />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+

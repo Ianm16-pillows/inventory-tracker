fc999366eb64545590c11d153ef11bd56b30e273
Ian Mwambuki <merlnadika@gmail.com>
2026-01-10 14:34:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import { Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Dashboard from "./pages/Dashboard"
-import Inventory from "./pages/Inventory"
-import AddItem from "./pages/AddItem"
-import EditItem from "./pages/EditItem"
-import Alerts from "./pages/Alerts"
+import { Routes, Route } from "react-router-dom";
+import Inventory from "./pages/Inventory";
+import AddItem from "./pages/AddItem";
+import EditItem from "./pages/EditItem";
+import Alerts from "./pages/Alerts";
+import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/" element={<Inventory />} />
         <Route path="/add" element={<AddItem />} />
         <Route path="/edit/:id" element={<EditItem />} />
         <Route path="/alerts" element={<Alerts />} />
       </Routes>
     </>
-  )
+  );
 }
-
-export default App

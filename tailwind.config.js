010845e50a807f7531f199e95c18d936fb58e5b0
Ian Mwambuki <merlnadika@gmail.com>
2026-01-10 15:20:05 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e3a8a',   // deep blue
+        secondary: '#059669'  // green
+      }
+    },
   },
   plugins: [],
-}
+};

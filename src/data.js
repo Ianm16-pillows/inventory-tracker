835378acb7293d0b2479b840876a92c191af4140
Ian Mwambuki <merlnadika@gmail.com>
2026-01-12 22:46:34 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const initialItems = [
+  { id: 1, name: "Sulphuric Acid", category: "Chemicals", quantity: 5 },
+  { id: 2, name: "Hydrochloric Acid (HCl)", category: "Chemicals", quantity: 8 },
+  { id: 3, name: "Methanol", category: "Chemicals", quantity: 6 },
+  { id: 4, name: "Ethanol", category: "Chemicals", quantity: 10 },
+
+  { id: 5, name: "TB Rapid Test Kit", category: "Rapid Tests", quantity: 120 },
+  { id: 6, name: "H. Pylori Test Kit", category: "Rapid Tests", quantity: 90 },
+  { id: 7, name: "HBSAG Test Kit", category: "Rapid Tests", quantity: 150 },
+  { id: 8, name: "Malaria Test Kit", category: "Rapid Tests", quantity: 200 },
+  { id: 9, name: "COVID-19 Test Kit", category: "Rapid Tests", quantity: 75 },
+
+  { id: 10, name: "Urine 10 Parameter Panel", category: "Urinalysis", quantity: 60 },
+  { id: 11, name: "Urine 2 Parameter Panel", category: "Urinalysis", quantity: 40 },
+  { id: 12, name: "COC–THC Drug Test", category: "Urinalysis", quantity: 25 },
+];
